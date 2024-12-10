528927546034f95ab5149f7e9d09c5d0cdff636f
Memby Poudel <officialmembypoudel@gmail.com>
2024-12-10 15:37:28 +0545

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import DashboardScreen from './screens/DashboardScreen';
+import GadgetsProvider from './context/GadgetsContext';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GadgetsProvider>
+      <DashboardScreen />
+    </GadgetsProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
